a3d0b9a8531608ac260290400a33f597c39f90ec
Dalois-30 <claudetapi@yahoo.com>
2024-07-04 02:22:22 +0100

    import ABI and start integrating contract

---
+++ src/context/TransactionContext.jsx
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { ethers } from "ethers"
+
+import { crowdfundingAddress,tokenAddress, crowdABI, tokenABI } from '../../utils/constants';
+// import { contractABI, contractAddress } from '../../utils/constants';
+
+export const TransactionContext = React.createContext();
+
+const { ethereum } = window;
+
+const getEtherumContract = async () => {
+    // create provider
+    const provider = new ethers.BrowserProvider(ethereum)
+
+    // get connected account
+    const signer = await provider.getSigner();
+
+    // create a new transaction contract
+    const crowdfundingContract = new ethers.Contract(crowdfundingAddress, crowdABI, signer)
+    const tokenContract = new ethers.Contract(tokenAddress, tokenABI, signer)
+
+    return {crowdfundingContract, tokenContract};
+}
+
+
+export const TransactionProvider = ({ children }) => {
+    const [currentAccount, setCurrentAccount] = useState('');
+    const [formData, setFormData] = useState({ addressTo: "", amount: "", keyword: "", message: "" });
+    const [isLoading, setIsLoading] = useState(false);
+    const [setTransactionCount] = useState(localStorage.getItem("transactionCount"));
+    const [transactions, setTransactions] = useState([]);
+
+    const handleChange = (e, name) => {
+        setFormData((prevState) => ({
+            ...prevState, [name]: e.target.value
+        }));
+    };
+
+    const checkIfWalletIsConnected = async () => {
+        try {
+            if (!ethereum) {
+                return alert("Please install Metamask");
+            }
+            const accounts = await ethereum.request({ method: "eth_accounts" });
+            if (accounts.length) {
+                setCurrentAccount(accounts[0]);
+            } else {
+                console.log("No account found");
+            }
+            console.log(accounts);
+        } catch (error) {
+            console.log(error);
+            throw new Error("No ethereum object.");
+        }
+    };
+
+    const connectWallet = async () => {
+        try {
+            if (!ethereum) {
+                return alert("Please install Metamask");
+            }
+            const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+            setCurrentAccount(accounts[0]);
+            return true
+        } catch (error) {
+            console.log(error);
+            // throw new Error("No ethereum object.");
+            return false
+        }
+    };
+
+    const sendTransaction = async (assetSymb, amount, assetIdForMint) => {
+        try {
+            if (!ethereum) {
+                return alert("Please install Metamask");
+            }
+            console.log("assetSymb", assetSymb);
+            console.log("amount", amount);
+            console.log("assetIdForMint", assetIdForMint);
+            setIsLoading(true);
+            const transactionContract = await getEtherumContract();
+            console.log("transactionContract", transactionContract);
+            const tx = await transactionContract.sendMintRequest('ASSER', ethers.parseEther(amount.toString()), assetIdForMint, currentAccount);
+            console.log(`Loading - ${tx.hash}`);
+            await tx.wait();
+            setIsLoading(false);
+            console.log("success", tx);
+
+            window.location.reload();
+        } catch (error) {
+            console.log(error);
+            throw new Error("No ethereum object.");
+        }
+    };
+
+    useEffect(() => {
+        checkIfWalletIsConnected();
+    }, []);
+
+    return (
+        <TransactionContext.Provider value={{ connectWallet, currentAccount, formData, handleChange, sendTransaction, transactions, isLoading }}>
+            {children}
+        </TransactionContext.Provider>
+    );
+};
+
+TransactionProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+import crowd from "./CrowndfundinManager.json";
+import token from "./Stablecoin.json";
+
+// export const contractAddress = "0xa446eb724d4452ee212C1747411DE542703F03aA";
+export const tokenAddress = "0x9E569C9deb9C9a1B9988368Db31B21B00f0c78d6";
+export const crowdfundingAddress = "0x5Fdb10dfBa9F5D4e71D2751d4ce4eE10ebd49934";
+export const crowdABI = crowd.abi;
+export const tokenABI = token.abi;
+
+
